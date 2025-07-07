057f91d6e5f4c0bdedf29502292a8b6e188f2343
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-07 09:35:13 -0300

    Add clsx dependency to package.json and update package-lock.json

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,12 @@
+type ContainerProps = {
+    children: React.ReactNode
+}
+
+
+export function Container({children}: ContainerProps){
+    return (
+        <div className="bg-slate-100 min-h-screen">
+      <div className="max-w-screen-lg mx-auto ">{children}</div>
+    </div>
+    )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <div className="flex justify-center">
+        <div className="bg-violet-500 w-[90%] h-20"></div>
+      </div>
+    </header>
+  );
+}
