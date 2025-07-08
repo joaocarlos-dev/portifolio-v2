e978f46ce69880b91bfc662573c0794572c0b15c
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-08 14:35:29 -0300

    Refactor NavBar and NavButton components for improved styling and structure

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,29 @@
-import { HamburgerIcon } from "lucide-react";
+import { LanguagesIcon, MenuIcon, MoonStarIcon } from "lucide-react";
 import { NavButton } from "../NavButton";
+import clsx from "clsx";
 
 export function NavBar() {
   return (
     <nav>
       <div className="flex justify-center mt-5">
-        <div className="bg-violet-500 w-[90%] h-16 rounded-3xl flex flex-row items-center">
+        <div
+          className={clsx(
+            "bg-violet-500",
+            "w-[90%] h-16 rounded-3xl",
+            "flex flex-row items-center justify-between"
+          )}
+        >
           <NavButton>
-            <HamburgerIcon />
+            <MenuIcon />
           </NavButton>
+          <div className="flex ">
+            <NavButton>
+              <LanguagesIcon />
+            </NavButton>
+            <NavButton>
+              <MoonStarIcon />
+            </NavButton>
+          </div>
         </div>
       </div>
     </nav>
--- src/components/NavButton/index.tsx
+++ src/components/NavButton/index.tsx
@@ -1,7 +1,17 @@
+import clsx from "clsx";
+
 type NavButtonProps = {} & React.ComponentProps<"button">;
 
-export function NavButton({}: NavButtonProps) {
+export function NavButton({ ...props }: NavButtonProps) {
   return (
-    <button className="bg-violet-100 w-10 h-10 ml-3 rounded-xl [&_svg]:w-10" />
+    <button
+      className={clsx(
+        "flex justify-center items-center",
+        "bg-violet-100 w-10 h-10 mx-3 rounded-xl",
+        "[&_svg]:text-violet-800",
+        "hover:cursor-pointer"
+      )}
+      {...props}
+    />
   );
 }
