f9efff25c1fa67a6df85b68664f6b5b102b7efc0
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-10 15:14:29 -0300

    Add NavButton component for reusable button functionality in NavBar

---
+++ src/components/NavBar/NavButton/index.tsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+
+type NavButtonProps = {} & React.ComponentProps<"button">;
+
+export function NavButton({ children, ...props }: NavButtonProps) {
+  return (
+    <button
+      className={clsx(
+        "flex justify-center items-center",
+        "bg-violet-100 w-10 h-10 mx-3 rounded-xl",
+        "[&_svg]:text-violet-800",
+        "hover:cursor-pointer",
+        "transition ease-in-out duration-400",
+        "hover:bg-violet-300"
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
