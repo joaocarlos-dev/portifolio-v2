388ec1234e6e911a4af8066047dafffa5fbabad5
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-15 10:48:41 -0300

    Enhance AboutPage styles for dark mode support

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,12 +7,12 @@ export default function AboutPage() {
     <div className="flex justify-center">
       <ContentContainer>
         <div className="flex flex-col items-center">
-          <h1 className="text-5xl pt-3 text-lime-400">
+          <h1 className="text-5xl pt-3 text-lime-400 dark:text-violet-700">
             <TranslatedText tKey={"about_hello"} />
           </h1>
           <p
             className={clsx(
-              "text-violet-100 ",
+              "text-violet-100 dark:text-dark-text",
               "text-justify indent-8 leading-7",
               "px-4 pt-2"
             )}
