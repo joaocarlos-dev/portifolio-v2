757eb10b7e1618f178ff70d0a6ef023961444e20
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-15 10:46:50 -0300

    Refactor AboutPage to use TranslatedText for internationalization; add about_text translation to both English and Portuguese locales

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,26 @@
 import { ContentContainer } from "@/components/ContentContainer";
+import { TranslatedText } from "@/components/TranslatedText";
+import clsx from "clsx";
 
 export default function AboutPage() {
   return (
     <div className="flex justify-center">
-      <ContentContainer>Teste nessa porra</ContentContainer>
+      <ContentContainer>
+        <div className="flex flex-col items-center">
+          <h1 className="text-5xl pt-3 text-lime-400">
+            <TranslatedText tKey={"about_hello"} />
+          </h1>
+          <p
+            className={clsx(
+              "text-violet-100 ",
+              "text-justify indent-8 leading-7",
+              "px-4 pt-2"
+            )}
+          >
+            <TranslatedText tKey={"about_text"} />
+          </p>
+        </div>
+      </ContentContainer>
     </div>
   );
 }
