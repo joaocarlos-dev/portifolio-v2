0be2dbbd60ccbd6fb041da6f2038ae03cb42dd11
ja1zinh0 <joao2011bha@hotmail.com>
2025-08-08 12:42:06 -0300

    added 2 more images to project

--- src/data/projectImages.ts
+++ src/data/projectImages.ts
@@ -1,8 +1,9 @@
-// src/data/projectImages.ts
 export const projectImages: Record<string, string[]> = {
   the_blog: [
     "/images/projects/the_blog/1.png",
     "/images/projects/the_blog/2.png",
     "/images/projects/the_blog/3.png",
+    "/images/projects/the_blog/4.png",
+    "/images/projects/the_blog/5.png",
   ],
 };
